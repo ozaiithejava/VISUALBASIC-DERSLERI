ca9b0a767b86827a2ac171dd2d205bacff1d6288
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-12-28 10:23:51 +0300

    Delete Ders1/sonsuzArgsexample.vb

--- Ders1/sonsuzArgsexample.vb
+++
@@ -1,8 +0,0 @@
-Sub SonsuzArgsFunc(ParamArray args() As Object)
-    For Each arg In args
-        Console.WriteLine(arg)
-    Next
-End Sub
-
-' Kullanım örneği:
-SonsuzArgsFunc(1, 2, 3, "a", "b", True)
